7f2e06a18e8b8030f534e6c27207f2206365c5da
rjoshi4 <rjoshi4@DIN16002769.corp.capgemini.com>
2019-04-08 16:48:34 +0530

    swagger added and app.prop changed

--- src/main/java/com/example/service/StudentServiceImpl.java
+++ src/main/java/com/example/service/StudentServiceImpl.java
@@ -3,30 +3,35 @@ package com.example.service;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.exception.StudentException;
 import com.example.model.Student;
 import com.example.repository.StudentDao;
 
 @Service
-public class StudentServiceImpl implements StudentService{
+public class StudentServiceImpl implements StudentService {
 
 	@Autowired
 	private StudentDao studentDao;
-	
+
 	@Override
+	@Transactional
+	@CachePut(value = "caching", key = "#student.rollno")
 	public Student createStudent(Student student) {
 		return studentDao.save(student);
 	}
-	
+
 	@Override
+	@Cacheable(value = "caching", key = "#id")
 	public Student getStudent(Integer id) throws StudentException {
 		Optional<Student> list = studentDao.findById(id);
-		if(list.isPresent() && id != null){
+		if (list.isPresent() && id != null) {
 			return list.get();
-		}
-		else{
+		} else {
 			throw new StudentException("Bank not found");
 		}
 	}
